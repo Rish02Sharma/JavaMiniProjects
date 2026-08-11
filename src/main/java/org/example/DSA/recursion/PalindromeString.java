d5b124662cd67ae21731d22bff3acb03e15c9ebc
Rishabh <rishabh@zilter.io>
2026-08-11 17:06:14 +0530

    is palindrome recursion

--- src/main/java/org/example/DSA/recursion/PalindromeString.java
+++ src/main/java/org/example/DSA/recursion/PalindromeString.java
@@ -2,12 +2,21 @@ package org.example.DSA.recursion;
 
 public class PalindromeString {
 
-    static void isPalindrome(){
+    static boolean isPalindrome(int s, int e, String newstr){
+        if(s==e && newstr.charAt(s) == newstr.charAt(e)) return true;
 
+        if(newstr.charAt(s) == newstr.charAt(e)){
+            return isPalindrome(++s, --e, newstr);
+        }else {
+            return false;
+        }
     }
 
     public static void main(String[] args){
         String s = "A man, a plan, a canal: Panama";
-
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "");
+        int len = s.length();
+        System.out.print("Is it palindrome" + isPalindrome(0, len-1, s));
     }
 }
